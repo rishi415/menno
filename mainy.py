eed9b2339143c60d154474df2b64cdcf30ce3d16
rishi415 <rishibasant415@gmail.com>
2018-03-02 18:14:29 +0100

    Update mainy.py

--- mainy.py
+++ mainy.py
@@ -65,4 +65,4 @@ def fixing():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='34.209.76.201', port=4000)
+    app.run(debug=True,host='0.0.0.0', port=80)
